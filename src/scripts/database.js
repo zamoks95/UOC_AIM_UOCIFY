5ddc1f711e27d7eee90eb7ff7ac2c20472080a1f
sergi.zamora <sergi.zamora.95@gmail.com>
2020-10-08 01:07:07 +0200

    created Database

---
+++ src/scripts/database.js
@@ -0,0 +1,198 @@
+export const artists = [
+    {
+        name: "empty",
+        image: "empty.jpg"
+    },
+    {
+        name: "beatles-fan-club",
+        image: "beatles-fan-club.jpg"
+    },
+    {
+        name: "the-beatles",
+        image: "the-beatles.jpg"
+    },
+    {
+        name: "the-beatles-acoustic-trio",
+        image: "the-beatles-acoustic-trio.jpg"
+    },
+    {
+        name: "the-beatles-complete",
+        image: "the-beatles-complete.jpg"
+    },
+    {
+        name: "the-beatles-connection",
+        image: "the-beatles-connection.jpg"
+    },
+    {
+        name: "the-beatles-covered-1",
+        image: "the-beatles-covered-1.jpg"
+    },
+    {
+        name: "the-beatles-interviews",
+        image: "the-beatles-interviews.jpg"
+    },
+    {
+        name: "the-beatles-recovered",
+        image: "the-beatles-recovered.jpg"
+    },
+    {
+        name: "the-beatles-revival-band",
+        image: "the-beatles-revival-band.jpg"
+    },
+    {
+        name: "the-beatles-revival-band-1",
+        image: "the-beatles-revival-band-1.jpg"
+    },
+    {
+        name: "the-beatles-tribute",
+        image: "the-beatles-tribute.jpg"
+    },
+    {
+        name: "the-beatles-tribute-band",
+        image: "the-beatles-tribute-band.jpg"
+    },
+    {
+        name: "the-beatles-with-tony-sheridan",
+        image: "the-beatles-with-tony-sheridan.jpg"
+    },
+    {
+        name: "the-bornagen-beatles",
+        image: "the-bornagen-beatles.jpg"
+    },
+    {
+        name: "the-new-beatles",
+        image: "the-new-beatles.jpg"
+    },
+    {
+        name: "the-silver-beatles",
+        image: "the-silver-beatles.jpg"
+    },
+    {
+        name: "the-sounds-of-the-beatles",
+        image: "the-sounds-of-the-beatles.jpg"
+    },
+    {
+        name: "the-ultimate-beatles-cover-band",
+        image: "the-ultimate-beatles-cover-band.jpg"
+    },
+    {
+        name: "tony-sheridan-and-the-beatles",
+        image: "tony-sheridan-and-the-beatles.jpg"
+    }
+]
+
+
+export const albums = [
+    {
+        name: "1",
+        image: "1.jpg"
+    },
+    {
+        name: "abbey-road",
+        image: "abbey-road.jpg"
+    },
+    {
+        name: "anthology2",
+        image: "anthology2.jpg"
+    },
+    {
+        name: "anthology3",
+        image: "anthology3.jpg"
+    },
+    {
+        name: "beatles-and-chill",
+        image: "beatles-and-chill.jpg"
+    },
+    {
+        name: "best-of",
+        image: "best-of.jpg"
+    },
+    {
+        name: "blackbird",
+        image: "blackbird.jpg"
+    },
+    {
+        name: "come-together",
+        image: "come-together.jpg"
+    },
+    {
+        name: "glee",
+        image: "glee.jpg"
+    },
+    {
+        name: "let-it-be",
+        image: "let-it-be.jpg"
+    },
+    {
+        name: "love",
+        image: "love.jpg"
+    },
+    {
+        name: "magical-mystery-tour",
+        image: "magical-mystery-tour.jpg"
+    },
+    {
+        name: "please-please-me",
+        image: "please-please-me.jpg"
+    },
+    {
+        name: "rubber-soul",
+        image: "rubber-soul.jpg"
+    },
+    {
+        name: "sgt-peppers",
+        image: "sgt-peppers.jpg"
+    },
+    {
+        name: "songs-of-the-beatles",
+        image: "songs-of-the-beatles.jpg"
+    },
+    {
+        name: "the-beatles",
+        image: "the-beatles.jpg"
+    },
+    {
+        name: "the-beatles-1",
+        image: "the-beatles-1.jpg"
+    },
+    {
+        name: "the-beatles-2",
+        image: "the-beatles-2.jpg"
+    },
+    {
+        name: "the-beatles-3",
+        image: "the-beatles-3.jpg"
+    },
+    {
+        name: "the-beatles-1962-1966",
+        image: "the-beatles-1962-1966.jpg"
+    },
+    {
+        name: "the-beatles-1967-1970",
+        image: "the-beatles-1967-1970.jpg"
+    },
+    {
+        name: "the-beatles-classics",
+        image: "the-beatles-classics.jpg"
+    },
+    {
+        name: "the-beatles-nova",
+        image: "the-beatles-nova.jpg"
+    },
+    {
+        name: "the-beatles-white-album",
+        image: "the-beatles-white-album.jpg"
+    },
+    {
+        name: "with-the-beatles",
+        image: "with-the-beatles.jpg"
+    },
+    {
+        name: "yellow-submarine",
+        image: "yellow-submarine.jpg"
+    },
+    {
+        name: "yellow-submarine-1",
+        image: "yellow-submarine-1.jpg"
+    },
+]
